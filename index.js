cb0d33bd3088b7d3a1461bb18be41206b9113f34
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2021-05-26 16:05:38 +0530

    understood what to do

---
+++ index.js
@@ -0,0 +1,10 @@
+// If the blood composition of the person is a subsequence of the virus composition V, then the person is  identified as POSITIVE otherwise NEGATIVE.
+// Scientists gave virus (V) of composition string (coronavirus). 
+// We have to take N number of samples (example input is 4)
+// The blood samples strings are subsequence or not, ("abcde" is subsequence to "coronavirus" or not). means this sample "abcde" is "NEGATIVE". 
+// Constraints (edge case)
+// 1<= N <=10
+// 1<= |B|<= |V|<= 10^5
+let main = () => {
+    virus = input
+}
